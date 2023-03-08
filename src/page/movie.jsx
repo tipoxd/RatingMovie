2197cd9aafa22744c6cacbab85fb793c9c58877d
Glicht <rosalesvallorani@gmail.com>
2023-03-08 07:44:12 -0400

    cambios

--- src/page/movie.jsx
+++ src/page/movie.jsx
@@ -10,6 +10,7 @@ const API_KEY = import.meta.env.VITE_API_KEY;
 
 export const Movie_data = () => {
     const [Jsonmovie, setJsonmovie] = useState([]);
+    
     const { movie_id } = useParams();
 
     async function Info_movie() {
@@ -38,7 +39,17 @@ export const Movie_data = () => {
                     </div>
                     <div className={`[ w-full gap-3 flex flex-col  p-3 rounded-lg ] [ lg:w-3/4 ] [ md:w-3/4 ] `}>
                         < div >
-                            <h1 className='[ text-lg font-bold text-primary ]'>{Jsonmovie.title}</h1>
+                            <div className='[ w-full flex  justify-between items-center ]'>
+                                <h1 className='[ text-lg font-bold text-primary ]'>{Jsonmovie.title}</h1>
+                                <div className='[ flex items-center gap-1 ]'>
+                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                        <path fillRule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clipRule="evenodd" />
+                                    </svg>
+                                    <label className='text-primary'>{Jsonmovie.vote_average}</label>
+                                </div>
+
+                            </div>
+
                             <p className='[ text-gray-400 ]'>{Jsonmovie.overview}</p>
                         </div>
                         <div className='[ flex flex-col gap-5 ] [ lg:flex-row ] [ md:flex-row ]'>
